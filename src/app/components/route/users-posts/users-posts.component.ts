8f1ad9d8cf4a2d170d9f79c6bb44bba43fedd128
agina <mohammed.awadallah@gmail.com>
2018-08-10 13:18:28 -0800

    Send Parameters via Html & TypeScript

--- src/app/components/route/users-posts/users-posts.component.ts
+++ src/app/components/route/users-posts/users-posts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CallApiService } from '../../../Services/call-api.service';
-import { Router } from '../../../../../node_modules/@angular/router';
+import { Router, ActivatedRoute } from '../../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-users-posts',
@@ -17,19 +17,30 @@ export class UsersPostsComponent implements OnInit {
   wantDelete = false;
   disabled = false;
   wantEdit = false;
+  id:number=1;
 
-  constructor(private call: CallApiService, private router: Router) { }
+  constructor(private call: CallApiService, private router: Router,private _route:ActivatedRoute) { }
 
   ngOnInit() {
 
     this.getPosts();
+    ////// Get Parameters as object parameters
+    // this._route.params.subscribe(params => {
+    //   this.id = params['id']
+     
+    // });
+    ///// Get parameters as  QueryParams 
+    this.id = this._route.snapshot.queryParams["id"];
 
   }
 
+  // getPosts() {
+  //   this.call.getPostsByUser(sessionStorage.getItem('id')).subscribe(
+  //     posts => { this.postsData = posts; this.ngOnInit(); })}
   getPosts() {
-    this.call.getPostsByUser(sessionStorage.getItem('id')).subscribe(
+    this.call.getPostsByUser(this.id).subscribe(
       posts => { this.postsData = posts; this.ngOnInit(); })}
-      
+       
 
   deleteItem(id) {
     this.call.deletePost(id).subscribe(
